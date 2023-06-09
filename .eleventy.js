572e40316e89651518309fd7a1c090e81e54309b
Mathieu Rudaz <mathieu.rudaz@tamedia.ch>
2023-06-09 15:41:48 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy({ "src/_root/*.*": "./" });
     eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/papers");
     eleventyConfig.addPassthroughCopy("src/program.js");
     eleventyConfig.addPassthroughCopy("src/program.csv");
     eleventyConfig.addPassthroughCopy("node_modules/d3-dsv/dist/d3-dsv.min.js");
